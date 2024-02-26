f85b22e27acf7f0d21061e7d9653ce12432fde71
SazonovV <sazvlad1@gmail.com>
2024-02-26 11:42:15 +0300

    Add reactions for bot

--- index.ts
+++ index.ts
@@ -2,8 +2,8 @@
 import { Client } from '@notionhq/client';
 import { CreatePageResponse } from "@notionhq/client/build/src/api-endpoints";
 import { botToken, notionToken, notionPages } from './settings.json';
+import TelegramBot, { Message } from 'node-telegram-bot-api';
 
-const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot(botToken, {polling: true});
 const notion = new Client({
   auth: notionToken
@@ -14,20 +14,20 @@ bot.onText(/\/chatId/, (msg: any) => {
   console.log(chatId)
 })
 
-bot.onText(/\/newDiscussion (.+)/, async (msg: any, match: string) => {
+bot.onText(/\/newDiscussion (.+)/, async (msg: Message, match: RegExpExecArray) => {
   const chatId = msg.chat.id;
   const username = msg.from.username
-  newDiscussion(chatId, username, match[1]);
+  newDiscussion(chatId, username, match[1], msg.message_id);
 
 })
 
-bot.onText(/\/nD (.+)/, async (msg: any, match: string) => {
+bot.onText(/\/nD (.+)/, async (msg: Message, match: RegExpExecArray) => {
   const chatId = msg.chat.id;
   const username = msg.from.username;
-  newDiscussion(chatId, username, match[1]);
+  newDiscussion(chatId, username, match[1], msg.message_id);
 })
 
-function newDiscussion(chatId: string, username: string, task: string) {
+function newDiscussion(chatId: number, username: string, task: string, msgId: number) {
   const dbId = notionPages[chatId]?.taskDB;
   if (!dbId) {
     bot.sendMessage(chatId, 'Notion DB не найдена')
@@ -35,7 +35,9 @@ function newDiscussion(chatId: string, username: string, task: string) {
     createTask(task, username, dbId)
       .then((createTaskResult) => {
         const createdTaskMessage = 'Новая тема для обсуждения - (https://www.notion.so/' + convertTaskToUrl(createTaskResult) + ')';
-        bot.sendMessage('275559199', createdTaskMessage)
+        const Reactions = [{ type: 'emoji', emoji: '👍' }];
+        (bot as any).setMessageReaction(chatId, msgId, { reaction: Reactions, is_big: true });
+        bot.sendMessage(275559199, createdTaskMessage);
       })
       .catch(e=> console.log(e))
   }
